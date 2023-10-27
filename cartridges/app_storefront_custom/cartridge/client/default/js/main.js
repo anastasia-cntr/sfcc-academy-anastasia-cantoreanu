81bddc54e32db4f3d72fda1b8b31b80961e573f1
CantoreanuAnastasia <anastasia.cntr@gmail.com>
2023-10-27 20:56:18 +0300

    feature/ex2-Using-JavaScript-to-hide/show-sections-on-click
    - added an anchor before ''Promo Code'' form
    - implemented a function that toggles the visibility of this form on click

--- cartridges/app_storefront_custom/cartridge/client/default/js/main.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/main.js
@@ -1,8 +1,20 @@
 window.jQuery = window.$ = require('jquery');
 var processInclude = require('./util');
+var clicked = 0;
 
 $(document).ready(function () {
     processInclude(require('../../../../../app_storefront_base/cartridge/client/default/js/main'));
     processInclude(require('./components/test'));
 });
 
+$(document).ready(function() {
+    $("#couponCodeAnchor").click(function() {
+        if (clicked == 0) {
+            $(".form-group.hidden").css("display", "block");
+            clicked = 1;
+        } else {
+            $(".form-group.hidden").css("display", "none");
+            clicked = 0;
+        }
+    });
+});
