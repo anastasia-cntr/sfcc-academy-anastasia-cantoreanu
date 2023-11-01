8c52cc14114b20a8ff10f46cc4309e4be9b51f17
CantoreanuAnastasia <anastasia.cntr@gmail.com>
2023-11-01 20:24:16 +0200

    feature/ex-7-Create-a-slot-type-Product-with-10-products
    - created a slot type product with 10 products
    - created a rendering template for the content slot
    - on large resolutions 2 products are displayed and 1 at small resolutions
    - ran it from a controller
    - included slots.xml file

--- cartridges/app_storefront_custom/cartridge/client/default/js/main.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/main.js
@@ -1,8 +1,24 @@
-window.jQuery = window.$ = require('jquery');
-var processInclude = require('./util');
+window.jQuery = window.$ = require("jquery");
+var processInclude = require("./util");
+var slick = require("slick-carousel");
 
-$(document).ready(function () {
-    processInclude(require('../../../../../app_storefront_base/cartridge/client/default/js/main'));
-    processInclude(require('./components/test'));
+$(document).ready(function() {
+    $(".homepage-product-listing.row").slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            }
+        ]
+    });
+    processInclude(require("../../../../../app_storefront_base/cartridge/client/default/js/main"));
+    processInclude(require("./components/test"));
 });
 
