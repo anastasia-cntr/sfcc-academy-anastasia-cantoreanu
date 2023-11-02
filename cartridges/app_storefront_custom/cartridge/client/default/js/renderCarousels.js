6874c9ce1e977199ba596c1a167cab57c46e6b45
CantoreanuAnastasia <anastasia.cntr@gmail.com>
2023-11-02 14:10:20 +0200

    feature/ex-6-Carousels-in-sync Fix#2
    - added back package.json file
    - removed $(document).ready(function()  from renderCarousels.js

--- cartridges/app_storefront_custom/cartridge/client/default/js/renderCarousels.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/renderCarousels.js
@@ -2,12 +2,10 @@
 
 var slick = require("slick-carousel");
 
-$(document).ready(function() {
-    $(".first-carousel").slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: true,
-        asNavFor: ".second-carousel"
-    });
+$(".first-carousel").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    asNavFor: ".second-carousel"
 });
